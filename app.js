01af277f9b4cc1d1e0f3421e56c4bc696e9b99b3
Arseny Prosvetov <arsprosvet@mail.ru>
2023-12-04 16:25:49 +0300

    calculator 1

---
+++ app.js
@@ -0,0 +1,52 @@
+const resultElement = document.getElementById("result");
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const submitBtn = document.getElementById("submit");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+let action;
+
+// console.log(typeof sum);
+plusBtn.onclick = function () {
+  action = "+";
+};
+minusBtn.onclick = function () {
+  action = "-";
+};
+
+function printResult(result) {
+  // if (result < 0) {
+  //   resultElement.style.color = "red";
+  // } else {
+  //   resultElement.style.color = "green";
+  // }
+  debugger;
+  result < 0
+    ? (resultElement.style.color = "red")
+    : (resultElement.style.color = "green");
+  resultElement.textContent = result;
+}
+
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+  const num1 = +inp1.value;
+  const num2 = +inp2.value;
+  // if (actionSymbol == "+") {
+  //   return num1 + num2;
+  // }
+  // if (actionSymbol == "-") {
+  //   return num1 - num2;
+  // }
+  return actionSymbol == "+" ? num1 + num2 : num1 - num2;
+}
+
+submitBtn.onclick = function () {
+  // if (action == "+") {
+  //   const sum = Number(input1.value) + Number(input2.value);
+  //   printResult(sum);
+  // } else {
+  //   const sum = Number(input1.value) - Number(input2.value);
+  //   printResult(sum);
+  // }
+  const result = computeNumbersWithAction(input1, input2, action);
+  printResult(result);
+};
---
+++ theory.js
@@ -0,0 +1,7 @@
+let productName = "iphone";
+let productPrice = 1000;
+let productQuantity = 34;
+let resultCost = productPrice * productQuantity;
+resultCost = console.log(
+  `Вы купили ${productName} (${productQuantity} шт.) по ${productPrice} ₽. Всего было потрачено: ${resultCost} ₽`
+);
